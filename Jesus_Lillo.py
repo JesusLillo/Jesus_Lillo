9fa3581e8a2c7ac4605e957bc0c414239ecfd94e
JesusLillo <je.lillo@duocuc.cl>
2024-04-12 15:57:25 +0000

    Jesus_Lillo_21.070.151-6 Evaluacion N°1 DRY7122

---
+++ Jesus_Lillo.py
@@ -0,0 +1,7 @@
+Nombre = input("Ingrese su nombre: ")
+Apellido = input("Ingrese su apellido: ")
+Rut = input("Ingrese su Rut: ")
+Duoc_mail = input("Ingrese su correo de Duoc: ")
+Duoc_sede = input ("Ingrese su sede correspondiente: ")
+print("Su nombre y apellido son " + Nombre, Apellido + " su rut es " + Rut + " Su correo Duoc es " + Duoc_mail + " y pertence a la sede " + Duoc_sede) 
+
